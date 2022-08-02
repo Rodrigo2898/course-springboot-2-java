fb269b17b392843395dbb0f54b8750b62fe08ff1
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2022-08-02 20:45:48 -0300

    User entity and resource

---
+++ src/main/java/com/testeWS/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.testeWS.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.testeWS.course.entities.User;
+
+@RestController  //recurso web, que é implementado por um controlador rest
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	// Método que vai ser um endpoint acessar os usuários
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
