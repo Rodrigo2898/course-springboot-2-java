b332d8cd92a271ab9ff67c2f05cc6eed4b9f673a
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2022-08-04 21:23:20 -0300

    Category

---
+++ src/main/java/com/testeWS/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.testeWS.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.testeWS.course.entities.Category;
+import com.testeWS.course.repositories.CategoryRepository;
+
+//Registrando o CategoryService como componente
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Integer id) {
+		Optional<Category>obj = repository.findById(id);
+		return obj.get();
+	}
+}
